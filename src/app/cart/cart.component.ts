4d65dc31b445f48feb0cb8902cc35019a06960e0
nunofhfontes <nunof.h.fontes@gmail.com>
2019-12-26 18:38:28 +0000

    Cart are now being displayed on Checkout page

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  items;
+
+  constructor(
+    private cartService: CartService
+  ){ }
 
   ngOnInit() {
+    this.items = this.cartService.getItems();
   }
 
 }
